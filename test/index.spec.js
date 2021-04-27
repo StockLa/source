1e904722408f0b2f331d307724f3b9cc843798d2
chengyil <cheng.yi@online-pajak.com>
2021-04-27 13:19:52 +0800

    setup docker file

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,6 @@
+const {Observable, from, interval} = require('rxjs');
 const {stockService} = require('@service');
+const {reduce, startWith} = require('../../../../home/chengyil/.cache/typescript/4.2/node_modules/rxjs/operators/index');
 describe('test', () => {
   it('should be true', () => {
     expect(true).toBe(true);
@@ -7,5 +9,11 @@ describe('test', () => {
   it('should be able to fetch Stock', () => {
     const stocks = stockService.getStock(['ibm']);
     stocks.subscribe(() => {});
+  });
+
+  it('should be able to concat', () => {
+    interval(100).pipe(
+        startWith({name: 'ibm', price: 1}),
+    );
   });
 });
